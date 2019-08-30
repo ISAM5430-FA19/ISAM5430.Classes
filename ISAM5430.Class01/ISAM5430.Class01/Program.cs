70d095ff94e60ca59e7cd18d928215d65d3cdf9d
Michael Wu <wum@uhcl.edu>
2019-08-29 21:24:51 -0500

    Initial commit

---
+++ ISAM5430.Class01/ISAM5430.Class01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; // java's import
+/*
+ multiline comment
+     */
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAM5430.Class01A
+{
+    class Program
+    {
+        // entry-point in the console app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Programming");
+        }
+    }
+}
