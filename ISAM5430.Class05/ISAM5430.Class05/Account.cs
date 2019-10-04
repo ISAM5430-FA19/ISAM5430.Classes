db5f182a16adbb5a2e3db89b377f98b8f53cc4fb
Michael Wu <wum@uhcl.edu>
2019-10-03 22:09:48 -0500

    Class 05

---
+++ ISAM5430.Class05/ISAM5430.Class05/Account.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISAM5430.Class05
+{
+    public class Account
+    {
+        private string _name;
+        public readonly bool Is_Active; // field
+        public bool IsActive
+        {
+            get; private set;
+        }
+        // Java, C#, C++
+        public Account(string name)
+        {
+            // D'Morgan's Law
+            // A || B == !(!A && !B)
+            // A && B == !(!A || !B)
+//            if (name != null && name != "")
+//            if (!(name == null || name == ""))
+//            if (!string.IsNullOrEmpty(name))
+                Name = name;
+            Is_Active = true;
+            IsActive = true;
+        }
+
+        public Account(string name, bool isActive)
+        {
+            Name = name;
+            IsActive = Is_Active = isActive;
+        }
+
+        public override string ToString()
+        {
+            // object 
+            // default implementation of object's ToString() method.
+            // return GetType().FullName; // prints out the fully-qualified type name
+//            return base.ToString();
+            if (IsActive)
+            {
+                return $"Active Account {Name}";
+            }
+            else
+            {
+                return $"Inactive Account {Name}";
+            }
+        }
+
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (value != null && value != "")
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        /*
+        public string get_Name()
+        {
+            return _name;
+        }
+
+        public void set_Name(string value)
+        {
+            _name = value;
+        } */
+    }
+}
---
+++ ISAM5430.Class05/ISAM5430.Class05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ISAM5430.Class05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constructor
+            // the default constructor that is parameterless
+            Account account = new Account("mike", false);
+            // a constructor forces you to initialize the required states
+            account.SetName("Mike");
+            Console.WriteLine("The name is " + account.GetName());
+            account.Name = "";
+            // account.set_Name("Wu");
+            // account.IsActive = true;
+            // account.Is_Active = true;
+
+            Console.WriteLine("Is the account active? " + account.IsActive);
+            Console.WriteLine("The name 2 is " + account.Name);
+            // Fully qualified type name, which includes a namespace "dot" type name
+            Console.WriteLine(account.ToString());
+            Console.WriteLine(account);
+        }
+    }
+}
