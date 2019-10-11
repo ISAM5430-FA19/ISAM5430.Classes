3c0531e9c6c8b8c5224e1d2f4742bdf250bb22e5
Michael Wu <wum@uhcl.edu>
2019-10-10 21:57:04 -0500

    class 06 commits

---
+++ ISAM5430.Class06/ISAM5430.Class06/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Math;
+
+namespace ISAM5430.Class06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PI);
+            Console.WriteLine(E);
+            Console.WriteLine(Sqrt(9));
+            Console.WriteLine(Math.Pow(10, 4));
+            Console.WriteLine(Math.Exp(4));
+            Console.WriteLine(Math.Exp(Math.Log(10) * 4));
+            Console.WriteLine(Math.Max(Math.Max(4, 5), 10));
+            Console.WriteLine(Max(4, 2, 5, 1));
+        }
+
+        public static int Max(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+
+        public static int Max(int a, int b, int c)
+        {
+            return Max(Max(a, b), c);
+        }
+
+        public static int Max(int a, int b, int c, int d)
+        {
+            return Max(Max(b, c, a), d);
+        }
+
+    }
+}
