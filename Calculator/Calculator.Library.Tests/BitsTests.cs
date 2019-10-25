983d22c94a26d6120b9c305e05629efc753692de
Michael Wu <wum@uhcl.edu>
2019-10-24 22:06:40 -0500

    Calculator base codes with unit tests

---
+++ Calculator/Calculator.Library.Tests/BitsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Calculator.Library.Tests
+{
+    class BitsTests
+    {
+        [Test]
+        public void TestConstructor1()
+        {
+            Bits bits = new Bits("101010");
+            string expected = "101010";
+            string actual = bits.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestConstructor2()
+        {
+            Bits bits = new Bits("101010", true);
+            string expected = "-101010";
+            string actual = bits.ToString();
+            Assert.AreEqual(expected, actual);
+
+            bits = new Bits("11111", false);
+            expected = "11111";
+            actual = bits.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestXor()
+        {
+            Bits a = new Bits("10101010");
+            Bits b = new Bits("11110000");
+            string expected = "1011010";
+            Bits c = a.Xor(b);
+            string actual = c.ToString();
+            Assert.AreEqual(expected, actual);
+
+            // Also check for +, - bits as well.
+        }
+    }
+}
---
+++ Calculator/Calculator.Library/Bits.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Library
+{
+    public class Bits
+    {
+        private readonly string _bits;
+        private readonly bool _signed;
+        public Bits(string bits, bool signed)
+            : this(bits)
+        {
+            _signed = signed;
+        }
+
+        public Bits(string bits)
+        {
+            _bits = bits;
+        }
+
+        public override string ToString()
+        {
+            return _signed ? $"-{_bits}" : _bits;
+        }
+
+        /// <summary>
+        /// Exclusive Or
+        /// 10101010
+        /// 11110000
+        /// 1011010
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Bits Xor(Bits other)
+        {
+            // this function is incomplete!
+            char[] a = this._bits.ToCharArray();
+            char[] b = other._bits.ToCharArray();
+            int len = a.Length > b.Length ? a.Length : b.Length; // maximum length
+            char[] c = new char[len];
+            for (int i = 0; i < len; i++)
+            {
+                int aIndex = a.Length - 1 - i;
+                int bIndex = b.Length - 1 - i;
+                char bitA;
+                char bitB;
+                bitA = aIndex >= 0 ? a[aIndex] : '0';
+                bitB = bIndex >= 0 ? b[bIndex] : '0';
+                if (bitA != bitB)
+                {
+                    c[i] = '1';
+                }
+                else
+                {
+                    c[i] = '0';
+                }
+            }
+            string s = new string(c);
+            Bits result = new Bits(s);
+            return result;
+        }
+    }
+}
